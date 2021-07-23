2f4c95f42345d04f6cd56818939ae8d2fbb59146
Eyal Eliav <eyal.eliav@forter.com>
2021-07-24 02:31:08 +0300

    add utils to send and receive files

--- server.c
+++ server.c
@@ -1,2 +1,24 @@
 #include "utils.h"
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
+int validate_user(char *username)
+{
+    DIR *dir = opendir(username);
+    if (dir)
+    {
+        closedir(dir);
+        return 1;
+    }
+    else if (ENOENT == errno)
+        return 0;
+    else
+        return -1;
+}
+
+
+int main()
+{
+    return 0;
+}
--- utils.c
+++ utils.c
@@ -4,9 +4,27 @@
 
 #define SIZE 200
 
-void send_file(FILE *fp, int sockfd){
+char * create_file_path(char * username, char * filename)
+{
+    char file_path[SIZE] = "";
+    strcat(file_path, username);
+    strcat(file_path, "/");
+    strcat(file_path, filename);
+    return file_path;
+}
+
+
+void send_file(char * username, char * filename, int sockfd){
     int n;
     char data[SIZE] = {0};
+    char * file_path = create_file_path(username, filename);
+    FILE * fp = fopen(file_path, "r");
+
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        exit(1);
+    }
 
     while(fgets(data, SIZE, fp) != NULL) {
         if (send(sockfd, data, sizeof(data), 0) == -1) {
@@ -15,14 +33,22 @@ void send_file(FILE *fp, int sockfd){
         }
         bzero(data, SIZE);
     }
+    return;
 }
 
-void write_file(int sockfd, char * filename){
+void write_file(char * username, char * filename, int sockfd){
     int n;
     FILE *fp;
     char buffer[SIZE];
+    char * file_path = create_file_path(username, filename);
+    fp = fopen(file_path, "w");
 
-    fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        exit(1);
+    }
+    
     while (recv(sockfd, buffer, SIZE, 0) > 0) {
         fprintf(fp, "%s", buffer);
         bzero(buffer, SIZE);
